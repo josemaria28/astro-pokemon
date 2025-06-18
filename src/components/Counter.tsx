5446e6774fa49fafef3c38b86eddf665a31d1018
José María <josemariagarciamoreno28@gmail.com>
2025-06-18 13:21:29 +0200

    componente favoritos pokemon

--- src/components/Counter.tsx
+++ src/components/Counter.tsx
@@ -1,11 +1,17 @@
-import { createSignal } from "solid-js";
+import { createSignal, type Component, type JSX } from "solid-js";
 
-export const Counter = () => {
-    const [count, setCount] = createSignal(10);
+interface Props {
+    initValue: number;
+    children?: JSX.Element;
+}
+
+export const Counter: Component<Props> = (props) => {
+    const [count, setCount] = createSignal(props.initValue);
 
     return (
         <>
-            <h1>Counter: {count()}</h1>
+            {props.children}
+            <p class="mt-7">Counter: {count()}</p>
             <button
                 onClick={() => setCount(prev => ++prev)}
                 class="bg-blue-500 p-2 mr-2 rounded text-amber-50 cursor-pointer">
